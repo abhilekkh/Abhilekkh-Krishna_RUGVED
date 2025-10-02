a0af11db434cf999331a6ce7a86d13159999839c
abhilekkh krishna <krishnabhilekkh@gmail.com>
2025-10-02 23:56:02 +0530

    Add Task 1

--- Task1/Q2.py
+++ Task1/Q2.py
@@ -1,9 +1,9 @@
-s=input("Enter the string: ")
-str_ls=sorted(s)
+s1=input("Enter the string: ")
+str_ls=sorted(s1)
 print("The sorted array is: ")
-for i in range(0,len(s)):
+for i in range(0,len(s1)):
     print(str_ls[i], end="")
 
 print("")
 for i in set(str_ls):
-     print(i+" frequency is: "+ str(s.count(i)))
+     print(i+" frequency is: "+ str(s1.count(i)))
